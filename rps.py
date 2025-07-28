87ef772e1c615afc6161ce0cbabac29696531f2c
mo8047 <itshassan45@gmail.com>
2025-07-28 23:36:55 +0400

    Update rps.py

--- rps.py
+++ rps.py
@@ -5,32 +5,58 @@ while u_input == "y":
     u_input = input("Rock, Paper, Scissors?: ")
     computer = ["Rock", "Paper", "Scissors"]
     
+
+    Rock = """
+        _______
+    ---'   ____)
+          (_____)
+          (_____)
+          (____)
+    ---.__(___)
+    """
+    
+    # Paper
+    Paper = """
+         _______
+    ---'    ____)____
+               ______)
+              _______)
+             _______)
+    ---.__________)
+    """
+    
+    # Scissors
+    Scissors = """
+        _______
+    ---'   ____)____
+              ______)
+           __________)
+          (____)
+    ---.__(___)
+    """
+    
+    ascii_art = {
+        "Rock":Rock,
+        "Paper":Paper,
+        "Scissors":Scissors
+    }
+    
     computer_rps = random.choice(computer)
     print(computer_rps)
     
-    if u_input == "Rock":
-        if computer_rps == "Rock":
-            print(f"Computer chose {computer_rps}: It's a draw")
-        elif computer_rps == "Paper":
-            print(f"Computer chose {computer_rps}: Computer wins")
-        elif computer_rps == "Scissors":
-            print(f"Computer chose {computer_rps}: User wins")
-        u_input = input("Would you like to play(y/n): ")
-    elif u_input == "Paper":
-        if computer_rps == "Rock":
-            print(f"Computer chose {computer_rps}: User wins")
-        elif computer_rps == "Paper":
-            print(f"Computer chose {computer_rps}: It's a draw")
-        elif computer_rps == "Scissors":
-            print(f"Computer chose {computer_rps}: Computer wins")
-        u_input = input("Would you like to play(y/n): ")
-    elif u_input == "Scissors":
-        if computer_rps == "Rock":
-            print(f"Computer chose {computer_rps}: Computer wins")
-        elif computer_rps == "Paper":
-            print(f"Computer chose {computer_rps}: User wins")
-        elif computer_rps == "Scissors":
-            print(f"Computer chose {computer_rps}: It's a draw")
-        u_input = input("Would you like to play(y/n): ")
+    if u_input == "Rock" and computer_rps == "Scissors":
+        print("User Wins")
+        #u_input = input("Would you like to play(y/n): ")
+    elif u_input == "Paper" and computer_rps == "Rock":
+        print("User Wins")
+        #u_input = input("Would you like to play(y/n): ")
+    elif u_input == "Scissors" and computer_rps == "Paper":
+        print("User Wins")
+        #u_input = input("Would you like to play(y/n): ")
     else:
-        u_input = input("Wrong input: Try Again?(y/n): ")
+        print("Computer Wins")
+    
+        
+    computer_art = ascii_art[computer_rps]
+    print(computer_art)
+    u_input = input("Would you like to play(y/n): ")
